c3cef76dd7fad36c5b425d0065e957e59422ba62
Kim Tran <tranhoang.thienkim.tk@gmail.com>
2024-05-31 19:33:26 +0800

    fork 4

--- client/src/components/Matchmaking.js
+++ client/src/components/Matchmaking.js
@@ -2,11 +2,18 @@ import TextField  from '@mui/material/TextField';
 import { Input } from '@mui/material';
 import TextareaAutosize from '@mui/material/TextareaAutosize';
 import { star } from '../assets/icons';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControl from '@mui/material/FormControl';
+import Checkbox from '@mui/material/Checkbox';
+import Radio from '@mui/material/Radio';
 const Matchmaking = () => {
   return (
     <>
     
-    <div className='flex relative flex-col items-center justify-center h-screen'>
+    <div className='ml-3 flex relative flex-col justify-center'>
   
     <div className="ml-10 py-10 flex items-center">
     <img 
@@ -40,11 +47,37 @@ const Matchmaking = () => {
     
     <TextField multiline rows = {4} id="outlined-basic" label="Anything that you want to see at your future studymate" variant="outlined" className="block w-full bg-transparent text-sm border-0 rounded-md focus:outline-none focus:outline-transparent p-2 ring-inset focus:ring-2 focus:ring-inset ring-1 disabled:text-slate-900 disabled:dark:text-slate-900 disabled:bg-slate-200 disabled:dark:bg-slate-800 disabled:cursor-not-allowed text-slate-700 dark:text-slate-900 placeholder:text-slate-900 placeholder:dark:text-slate-900 ring-slate-300 dark:ring-slate-700 focus:ring-red-950 focus:dark:ring-red-900 shadow-xl"/>
     </form>
+    <div className='space-x-96 py-5'>
+    <FormControl>
+  <span className= " text-red-900 font-bold font-palanquin" id="demo-radio-buttons-group-label"> Current Academic Level</span>
+  <RadioGroup
+    aria-labelledby="demo-radio-buttons-group-label"
+    defaultValue="not really good"
+    name="radio-buttons-group"
+  >
+    <FormControlLabel value="not really good" control={<Radio />} label="Not Really Good" />
+    <FormControlLabel value="good" control={<Radio />} label="Good" />
+    
+  </RadioGroup>
+</FormControl>
+<FormControl>
+  <span className= " text-red-900 font-bold font-palanquin" id="expect"> Study Goal</span>
+  <RadioGroup
+    aria-labelledby="expect"
+    defaultValue="A- and above"
+    name="expectations"
+  >
+    <FormControlLabel value="A- and above" control={<Radio />} label="A- and above" />
+    <FormControlLabel value="None" control={<Radio />} label="None" /> 
+  </RadioGroup>
+</FormControl>
+</div>
     <div class="text-center p-2 w-full">
           <button className ="inline-flex items-center justify-center py-2.5 px-5 gap-x-2 text-base whitespace-nowrap  rounded-md font-medium transition-colors focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 text-slate-50 dark:text-slate-950 bg-red-500 dark:bg-red-500 outline-indigo-500 dark:outline-indigo-500 hover:bg-red-300 hover:dark:bg-red-300 active:bg-red-900 active:dark:bg-red-900 focus-visible:bg-red-900 focus-visible:dark:bg-red-900 disabled:text-white disabled:border-transparent disabled:bg-slate-300 disabled:dark:bg-slate-700 disabled:cursor-not-allowed shadow-xl">
             <div>Submit</div>
           </button>
         </div>
+        
     </section>
     </div>
     </>
