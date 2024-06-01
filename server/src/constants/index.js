4ae0923ab004ee28038c37c2feb3fd2c9875b4fb
LeThiHongMinh <hongminhle2005@gmail.com>
2024-06-01 20:30:07 +0700

    Vercel

--- server/src/constants/index.js
+++ server/src/constants/index.js
@@ -1,9 +1,9 @@
-const { config } = require('dotenv')
-config()
+const { config } = require('dotenv');
+config();
 
 module.exports = {
   PORT: process.env.PORT,
-  SERVER_URL: process.env.SERVER_URL,
   CLIENT_URL: process.env.CLIENT_URL,
-  SECRET: process.env.SECRET,
-}
+  SERVER_URL: process.env.SERVER_URL,
+  SECRET: process.env.SECRET
+};
