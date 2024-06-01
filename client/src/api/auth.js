c8cd6af624d5f936c84ddec9c7fd3a88dec3c8f4
LeThiHongMinh <hongminhle2005@gmail.com>
2024-06-01 22:24:00 +0700

    render

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -3,19 +3,19 @@ axios.defaults.withCredentials = true
 
 export async function onRegistration(registrationData) {
   return await axios.post(
-    'https://orbital-lake.vercel.app/api/register',
+    'https://orbital-kq4q.onrender.com/api/register',
     registrationData
   )
 }
 
 export async function onLogin(loginData) {
-  return await axios.post('https://orbital-lake.vercel.app/api/login', loginData)
+  return await axios.post('https://orbital-kq4q.onrender.com/api/login', loginData)
 }
 
 export async function onLogout() {
-  return await axios.get('https://orbital-lake.vercel.app/api/logout')
+  return await axios.get('https://orbital-kq4q.onrender.com/api/logout')
 }
 
 export async function fetchProtectedInfo() {
-  return await axios.get('https://orbital-lake.vercel.app/api/protected')
+  return await axios.get('https://orbital-kq4q.onrender.com/api/protected')
 }
