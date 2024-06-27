0d2c30217a0209573f100d233f6fb395cb035ebd
LeThiHongMinh <hongminhle2005@gmail.com>
2024-06-27 23:13:46 +0700

    back

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg')
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'orbital',
-  password: 'Hm:200605',
-  port: 5432,
+  connectionString: process.env.POSTGRES_URL,
 })
+
+const connect = async () => {
+  const client = await pool.connect();
+  return client;
+};
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
